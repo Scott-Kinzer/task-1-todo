f279dd52fd655219cbe14b1822f92d55be549b65
Scott-Kinzer <zaz765831@gmail.com>
2023-07-25 16:31:48 +0300

    feat: added try catch block

--- helpers/formatDate.js
+++ helpers/formatDate.js
@@ -1,9 +1,13 @@
 export const formatDate = (date) => {
-    const month = date.toLocaleString('default', { month: 'long' });
-    const day = date.getDate();
-    const year = date.getFullYear();
+    try {
+        const month = date.toLocaleString('default', { month: 'long' });
+        const day = date.getDate();
+        const year = date.getFullYear();
+        
+        const formattedDate = `${month} ${day}, ${year}`;
     
-    const formattedDate = `${month} ${day}, ${year}`;
-
-    return formattedDate;
-}
+        return formattedDate;
+    } catch {
+        return "Wrong date";
+    }
+};
